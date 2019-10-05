49fb20426a5cc64fdf305f64c2a3bba993609afe
ketayon <ketayon@gmail.com>
2019-10-05 22:35:47 +0300

    make it better

---
+++ landing/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .forms import SubscriberForm
+
+
+def landing(request):
+    form = SubscriberForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        new_form = form.save()
+
+    return render(request, 'landing/landing.html', locals())
