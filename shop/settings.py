b693e5f530b9cf6adfa8e7efa2cd77ad84b3ec02
ketayon <ketayon@gmail.com>
2019-10-05 22:59:26 +0300

    make it better

--- shop/settings.py
+++ shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8lyhzvl&$@4ddxjgswttt4kkx-eq(9km5ry^95rukbs-6yq*^!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['comming-soon.herokuapp.com', 'localhost', '127.0.0.1']
 
